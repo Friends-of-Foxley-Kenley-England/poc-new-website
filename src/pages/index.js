dc6248ef110dfecd596f80cca67d81224e5504a3
Eloise Taylor <rockspud5693@gmail.com>
2021-07-06 11:56:13 +0100

    Working w3w implementation

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import {What3wordsAddress} from "@what3words/react-components"
 
 const HomeIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
@@ -12,6 +13,7 @@ const HomeIndex = ({ data, location }) => {
         <p>
           Volunteer group helping to manage the Foxley woods in Kenley, Surrey. 
         </p>
+        <What3wordsAddress words="tune.then.global" icon-color="#3CABAA" tooltip-location="entrance at Burwood Avenue"/>
       </Layout>
   )
 }
