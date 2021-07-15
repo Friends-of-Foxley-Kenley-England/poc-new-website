32009606e1b67d8b0b7e515f23b795fbb37ee794
eloisetaylor5693 <eloisetaylor5693@gmail.com>
2021-07-15 15:52:34 +0100

    Added page for ancient beech tree

---
+++ src/pages/ancient_beech_tree.js
@@ -0,0 +1,97 @@
+import * as React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const ContactIndex = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata?.title || `Title`
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <Seo title="ancient beech tree" />
+      <h1>Ancient Beech Tree</h1>
+      <h2>Reasons for the path diversion and fencing around the Ancient Beech</h2>
+    
+      <p>
+        The Ancient Beech pollard on the lower path in Foxley Wood ["Mother Beech", Tree ID No. 48125 in the Ancient Tree Inventory] 
+        has been a cause for concern to Croydon Council for some years, due to an area of basal decay not normally visible to 
+        passers-by as it is on the other side of the tree from the path. The condition of the tree has lead to a decision to divert 
+        the path out of the area into which it, or branches from it, could fall, and to fence the area in order to minimise any risk 
+        of harm which the tree may pose to users of Foxley Wood. As the tree is 23m tall, the exclusion circle must be 23m in radius. 
+        Exclusion will also enable compacted soils around the tree to recover their structure. This way of protecting ancient trees is 
+        increasingly used on conservation sites elsewhere, to maintain their habitat and visual value rather than fell them.
+      </p>
+      <p>
+        The decay may have been caused by the pruning of a large limb, the cavity shows fruiting bodies [brackets] of Ganoderma 
+        australe [Southern Bracket] and has in the past also had both forms of Kretzschmaria deusta [Brittle Cinder Fungus]. That the 
+        latter has not been seen fruiting for several years may be a hopeful sign that resources available to the fungus may have been 
+        used up and the decay compartmentalised [reference: CODIT]. Both of these fungi degrade the lignin in wood which provides 
+        structural rigidity. The tree can sense the increase in flexing and can respond by growing compensatory wood, of greater 
+        strength and density than normal wood. This can be seen in the arch of new wood around the cavity, which may actually be 
+        stronger than the original wood structure of the trunk. However, it is not known how far any decay may have extended up into 
+        the main branches, which could break out from the pollard union.
+      </p>
+      <p>
+        The tree was reduced in height by pruning several years ago to bring the crown below those of surrounding trees and reduce the 
+        potentially destructive effect of wind loading. Beech, especially over-mature specimens, are conventionally thought to respond 
+        poorly to pruning, but the regrowth of epicormic shoots along the pruned branches has been surprisingly good, creating a new 
+        lower, inner crown of new foliage which is a most hopeful sign of the continuing vigour of the tree and bodes well for an 
+        extended old age. Beech do not normally live more than 200-250 years, though pollarding can extend life by up to double the 
+        normal. The age of the tree was estimated 15 years ago [using the method in the Forestry Commission Technical Information 
+        Note:- "Estimating the Age of Large and Veteran Trees in Britain"] at 260 years, so now is estimated as 275 years.
+      </p>
+      <p>
+        Rather than remove the risk of harm to users of the wood by felling the tree, it’s extraordinary value as an ancient tree; the 
+        biodiversity of the community of organisms to which it is a host, and an ecology in itself; its status as a local icon of the 
+        wood, and as a veteran tree registered on the national database; grant funding from the Mayor of London’s Greener City Fund was 
+        applied for and won to create the path diversion and install fencing. The existing rolled stone path will also be removed and 
+        the soil around the tree will be improved and decompacted by adding a mixture of composted leaf mould and charcoal fines 
+        [biochar]. This is intended to improve the condition of the rooting zone of the tree with natural materials to assist its 
+        defence against the decay fungi. The work will be carried out by a contractor, assisted by the Friends and by Croydon TCV 
+        between the 5th and 16th of October. This will entail closure of the path.
+      </p>
+      <p>
+        The decay may have been caused by the pruning of a large limb, the cavity shows fruiting bodies [brackets] of Ganoderma 
+        australe [Southern Bracket] and has in the past also had both forms of Kretzschmaria deusta [Brittle Cinder Fungus] That the 
+        latter has not been seen fruiting for several years may be a hopeful sign that resources available to the fungus may have been 
+        used up and the decay compartmentalised [reference: CODIT]. Both of these fungi degrade the lignin in wood which provides 
+        structural rigidity. The tree can sense the increase in flexing and can respond by growing compensatory wood, of greater 
+        strength and density than normal wood. This can be seen in the arch of new wood around the cavity, which may actually be 
+        stronger than the original wood structure of the trunk. However, it is not known how far any decay may have extended up into 
+        the main branches, which could break out from the pollard union.In clearing the proposed route for the diversion, three badger 
+        holes were found adjacent to, and one directly in the route. Monitoring of these holes over several months, including with trail 
+        cameras, showed that only one is active and this is concealed behind a fallen tree. Advice was obtained from the East Surrey 
+        Badger Protection Society which has inspected the site and assisted in obtaining the necessary authorisation and licence from 
+        Natural England for work to take place adjacent to a sett.
+      </p>
+      <p>
+        It is understood and expected that restricting access to such a well-loved tree will be distressing to many users, some of whom 
+        have described it to the Friends as ‘’their’’ tree. So many people have engaged closely with the bark, the trunk, climbed it and 
+        swung from the branch swings, admired and revelled in it’s glory, brought children to experience its extraordinary character. But 
+        we need to protect the tree, and the users, who will at least still be able to see it, even if at something of a distance, via 
+        vistas we will create through the surrounding trees, and from re-sited benches, hopefully for many more years to come. The only 
+        alternative at present is to fell it, and no-one wants that….
+      </p>
+      <p>
+        If you have any concerns or questions, please contact the Friends of Foxley via this site or our Facebook page 
+        [Friends of Foxley Wood].
+      </p>
+      <p>
+        Written by <strong>Anthony Mills, Dip.Hort[RHS]</strong>; Tech Arbor.A.. Arboricultural Consultant for Friends of Foxley 19th Oct.20
+      </p>
+    </Layout>
+  )
+}
+
+export default ContactIndex
+
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
