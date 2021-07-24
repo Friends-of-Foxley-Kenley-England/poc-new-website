79bee8972e384689a9567132d27986cfd20cace9
eloisetaylor5693 <eloisetaylor5693@gmail.com>
2021-07-24 17:20:19 +0100

    Fixed logic to load the posts in the correct area (eg news posts in the news URL path etc)

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,11 +2,11 @@ const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.createPages = async ({ graphql, actions, reporter }) => {
-  const createPage = actions
+  const { createPage } = actions
 
   // Define a template for blog post
-  const blogPost = path.resolve(`./src/templates/blog-post.js`)
-  const workDayPost = path.resolve(`./src/templates/work-day-information.js`)
+  const blogPostTemplate = path.resolve(`./src/templates/blog-post.js`)
+  const workDayPostTemplate = path.resolve(`./src/templates/work-day-information.js`)
 
   // Get all markdown blog posts sorted by date
   const result = await graphql(
@@ -36,66 +36,73 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     return
   }
 
-  console.log('result.data', result.data)
-  console.log('result', result)
-  const posts = result.data.allMarkdownRemark.nodes.filter(x => x.type === `news`)
-  const workdayPosts = result.data.allMarkdownRemark.nodes.filter(x => x.type === `work-days`)
+  const posts = getPosts(result)
 
-  console.log('news',posts)
-  console.log('workdayPosts',workdayPosts)
-
-  // Create blog posts pages
-  // But only if there's at least one markdown file found at "content/blog" (defined in gatsby-config.js)
-  // `context` is available in the template as a prop and as a variable in GraphQL
-
-  createPostPage(posts, blogPost, createPage)
-  createPostPage(workdayPosts, workDayPost, createPage)
- 
+  createPostPage(posts.news, blogPostTemplate, 'news', createPage)
+  createPostPage(posts.workdays, workDayPostTemplate, 'work-days', createPage) 
 }
 
-function createPostPage(posts, component, createPage){
-  if (posts.length > 0) {
-    posts.forEach((post, index) => {
-      const previousPostId = index === 0 ? null : posts[index - 1].id
-      const nextPostId = index === posts.length - 1 ? null : posts[index + 1].id
+function getPosts(result) {
+      const news = result.data.allMarkdownRemark.nodes.filter(x => x.fileAbsolutePath.includes(`/content/news/`))
+      const workdays = result.data.allMarkdownRemark.nodes.filter(x => x.fileAbsolutePath.includes(`work-days`))
 
-      createPage({
-        path: post.fields.slug,
-        component: component,
-        context: {
-          id: post.id,
-          previousPostId,
-          nextPostId,
-        },
-      })
-    })
-  }
-}
+      return {
+        news: news,
+        workdays: workdays
+      };
+    }
+
+
+    /***
+     * @summary creates the pages with post content (either news or workdays)
+     * @param {array} posts - list of posts to create pages for
+     * @param {string} template - path of the template to use to load the content in
+     * @param {string} parentPage - prefix for the URL so it's `news/hello` instead of just `hello`
+     */
+function createPostPage(posts, template, parentPage, createPage) {
+      if (posts.length > 0) {
+        posts.forEach((post, index) => {
+          const previousPostId = index === 0 ? null : posts[index - 1].id
+          const nextPostId = index === posts.length - 1 ? null : posts[index + 1].id
+
+          createPage({
+            path: parentPage + post.fields.slug,
+            component: template,
+            context: {
+              id: post.id,
+              previousPostId,
+              nextPostId,
+              //pagePath: path
+            },
+          })
+        })
+      }
+    }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
+      const { createNodeField } = actions
 
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
+      if (node.internal.type === `MarkdownRemark`) {
+        const value = createFilePath({ node, getNode })
 
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
-    })
-  }
-}
+        createNodeField({
+          name: `slug`,
+          node,
+          value,
+        })
+      }
+    }
 
 exports.createSchemaCustomization = ({ actions }) => {
-  const { createTypes } = actions
+      const { createTypes } = actions
 
-  // Explicitly define the siteMetadata {} object
-  // This way those will always be defined even if removed from gatsby-config.js
+      // Explicitly define the siteMetadata {} object
+      // This way those will always be defined even if removed from gatsby-config.js
 
-  // Also explicitly define the Markdown frontmatter
-  // This way the "MarkdownRemark" queries will return `null` even when no
-  // blog posts are stored inside "content/blog" instead of returning an error
-  createTypes(`
+      // Also explicitly define the Markdown frontmatter
+      // This way the "MarkdownRemark" queries will return `null` even when no
+      // blog posts are stored inside "content/blog" instead of returning an error
+      createTypes(`
     type SiteSiteMetadata {
       author: Author
       siteUrl: String
@@ -129,13 +136,13 @@ exports.createSchemaCustomization = ({ actions }) => {
       slug: String
     }
   `)
-}
+    }
 
 
 
-/* 
+/*
 
-Can access data:   
+Can access data:
 
 {
   allMarkdownRemark(
