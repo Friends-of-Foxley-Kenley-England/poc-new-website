a19b6818a2728eb6a5fddbd5b3fa2014f304ea1c
Eloise Taylor <rockspud5693@gmail.com>
2021-07-26 21:15:06 +0100

    added a page for the english oak

---
+++ src/pages/trees/english-oak.js
@@ -0,0 +1,81 @@
+import * as React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const EnglishOakTreeIndex = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata?.title || `Title`
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <Seo title="English Oak Tree" />
+      <h1>English Oak Tree</h1>
+      <h2>Other names - Common Oak, Pedunculate Oak, European Oak</h2>
+    
+      <p>
+        The English Oak is native to most of Western Europe. It is found in mixed woodland throughout the UK and is most common in 
+        the South and East. It’s an important feature of the English landscape, renowned for its longevity and noted for its 
+        distinctive leaves and groups of acorns. It is a long-lived tree with some examples over a thousand years old but in 
+        woodland, where it can grow to over 40m, it usually has a lifespan of about 300 years.
+      </p>
+      <p>
+        The leaves have 5-7 pairs of lobes, forming a typical wavy-edged outline; the upper surface is dark green and the underside 
+        is paler. It is closely related to the Sessile Oak (Quercus petraea).  The leaves of the English Oak have very short or no 
+        stalk and the acorns long ones. The Sessile Oak is the opposite - its leaves have stalks but the acorns don't. This is 
+        where the names pedunculate and sessile come from.
+      </p>
+      <p>
+        The English Oak is very important for wildlife: Numerous insects live on the leaves, buds, and in the acorns and it supports 
+        the highest biodiversity of insect herbivores of any British plant (more than 400 species). The acorns form a valuable food 
+        resource for several small mammals, notably squirrels, and some birds, including Jays.
+      </p>
+      <p>
+        The English oak has assumed the status of a national emblem and is woven into history and folklore. Druids would worship in 
+        oak groves, couples would marry under their spreading branches and the Yule Log, decorated for Christmas with holly and 
+        mistletoe, was traditionally cut from oak.
+      </p>
+      <p>
+          Oaks produce one of the hardest and most durable timbers on the planet and the English Oak has always enjoyed a close 
+          association with the Royal Navy, whose ships were constructed from oak timbers until the middle of the 19th century, earning 
+          the Navy the nickname ‘the Wooden Walls of Old England’. Oak is still used in construction and to make barrels for wine and spirits.
+      </p>
+
+      <h2>Oaks in Foxley Wood</h2>
+      <p>
+        Most of of the oaks in the wood are in the higher parts, which is Ancient Woodland and has been here since before 1600. However, 
+        there are very few trees more than about 200 years old, indicating that the wood was managed for timber and underwood even then.
+      </p>
+      <p>
+        Both species and hybrids are present and there are several Sessile Oaks in Woodland Way. The oldest are pollards, especially the 
+        one in the narrow belt of woodland between Higher Drive and the Recreation Ground. The Friends of Foxley have planted many of both 
+        species of oaks in glades in the wood made by re-coppicing hazel or where trees have fallen making gaps in the canopy, and where 
+        the coppicing of the secondary woodland has been done below the lower path. The Friends have also cut wood from fallen trees to 
+        make boards for the benches. The wood from the 900 year old Roke Oak in Roke Road (removed for housing development) was rescued 
+        and brought here to provide continuity of habitat for any specialist organisms to which it was host.
+      </p>
+      <h2>Fun Facts</h2>
+      <ul>
+          <li>Only 1 in 10,000 acorns grow up to be an oak tree</li>
+          <li>"The Royal Oak" is the third most popular pub name in the UK</li>
+          <li>There have been eight warships called HMS Royal Oak, and ‘Heart of Oak’ is the official march of the Royal Navy</li>
+      </ul>
+      <p></p>
+      <p>
+        Written by <strong>Anthony Mills, Dip.Hort[RHS]</strong>; Tech Arbor.A.. Arboricultural Consultant for Friends of Foxley
+      </p>
+    </Layout>
+  )
+}
+
+export default EnglishOakTreeIndex
+
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
