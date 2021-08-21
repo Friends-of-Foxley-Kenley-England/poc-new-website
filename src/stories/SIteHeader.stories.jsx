309a5c4a4842d9ea09ed4f81de41d6994668ef0f
Eloise Taylor <eloisetaylor5693@gmail.com>
2021-08-21 17:34:47 +0100

    Configured storybook

---
+++ src/stories/SIteHeader.stories.jsx
@@ -0,0 +1,37 @@
+// import React from 'react';
+// import SiteHeader from "../components/site-header"
+
+// export default {
+//   title: 'components/SiteHeader',
+//   component: SiteHeader,
+// };
+
+// export const Header = () => <SiteHeader
+//   className="Heading"
+//   menuLinks={[
+//     {
+//        name:'Home',
+//        link:'/'
+//     },
+//     {
+//        name:'Latest News',
+//        link:'/news'
+//     },
+//     {
+//        name:'Work Days',
+//        link:'/work-days'
+//     },
+//     {
+//        name:'History',
+//        link:'/history'
+//     },
+//     {
+//       name:'Trees',
+//       link:'/trees'
+//    },
+//     {
+//        name:'Contact',
+//        link:'/contact'
+//     }
+//   ]}
+//   siteTitle="site title" />;
---
+++ src/stories/Trees.stories.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import TreesIndex from './../pages/trees';
+
+export default {
+  title: 'Fof/Pages/Trees',
+  component: Page,
+};
+
+const Template = (args) => <TreesIndex/>;
