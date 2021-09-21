0f7386985d08e2af67d63fcf9aeda56917010a9b
Eloise Taylor <eloisetaylor5693@gmail.com>
2021-09-21 22:06:33 +0100

    #24 created page at the expected url

---
+++ src/pages/pages/ancient_beech_tree.js
@@ -0,0 +1,11 @@
+import AncientBeechTreeIndex from "../ancient_beech_tree";
+
+export default AncientBeechTreeIndex;
+
+/*
+
+    This page exists to preserve a URL from the old website.
+
+    A few leaflets, a QR code and other content may have this url
+
+*/
