60b7919e1d53076a957890c890e7de6f8dea7192
eloisetaylor5693 <eloisetaylor5693@gmail.com>
2022-02-13 12:55:12 +0000

    Added facebook image to the footer

--- src/components/site-footer.js
+++ src/components/site-footer.js
@@ -1,21 +1,35 @@
 import React from "react";
 import * as style from "./site-footer.module.css";
 import ExternalLink from "./external-link";
+import { StaticImage } from "gatsby-plugin-image";
 
 const SiteFooter = () => (
   <footer className={style.siteFooter}>
-    © {new Date().getFullYear()}, Built with
-    {` `}
-    <ExternalLink href="https://www.gatsbyjs.com" className={style.footerLink}>
-      Gatsby
-    </ExternalLink>
-    {` `}
-    by{" "}
-    <ExternalLink
-      href="https://github.com/eloisetaylor5693"
-      className={style.footerLink}>
-      Eloise Taylor
-    </ExternalLink>
+    <StaticImage
+      src="../images/facebook/FindUs-FB-RGB-Wht.svg"
+      alt="find us on facebook"
+      className={style.facebookImage}
+      loading="lazy"
+      objectFit="contain"
+      placeholder="tracedSVG"
+    />
+
+    <span className={ style.builtBy }>
+      © {new Date().getFullYear()}, Built with
+      {` `}
+      <ExternalLink
+        href="https://www.gatsbyjs.com"
+        className={style.footerLink}>
+        Gatsby
+      </ExternalLink>
+      {` `}
+      by{" "}
+      <ExternalLink
+        href="https://github.com/eloisetaylor5693"
+        className={style.footerLink}>
+        Eloise Taylor
+      </ExternalLink>
+    </span>
   </footer>
 );
 
