90bf3c4ab3236daa125a7049740e148cd0380381
eloisetaylor5693 <eloisetaylor5693@gmail.com>
2022-02-17 22:14:53 +0000

    updated news template

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -7,26 +7,23 @@ import Seo from "../components/seo";
 import * as style from "./blog-post.module.css";
 
 const BlogPostTemplate = ({ data, location }) => {
-  const post = data.markdownRemark;
+  const post = data.contentfulNews;
   const siteTitle = data.site.siteMetadata?.title || `Title`;
   const { previous, next } = data;
 
   return (
     // <script src="https://assets.what3words.com/sdk/v3/what3words.js"></script>
     <Layout location={location} title={siteTitle}>
-      <Seo
-        title={post.frontmatter.title}
-        description={post.frontmatter.description || post.excerpt}
-      />
+      <Seo title={post.title} description={post.newsContent.raw || post.excerpt} />
       <article
         className="blog-post"
         itemScope
         itemType="http://schema.org/Article">
         <header>
           <h1 className={style.blogPostHeader} itemProp="headline">
-            {post.frontmatter.title}
+            {post.title}
           </h1>
-          <p className={style.blogPostPostedDate}>{post.frontmatter.date}</p>
+          <p className={style.blogPostPostedDate}>{post.createdAt}</p>
         </header>
         <section
           dangerouslySetInnerHTML={{ __html: post.html }}
@@ -38,24 +35,24 @@ const BlogPostTemplate = ({ data, location }) => {
           <Bio />
         </footer>
       </article>
-      <nav className={style.blogPostNav}>
+      {/* <nav className={style.blogPostNav}>
         <ul className={style.blogPostNavLinks}>
           <li>
             {previous && (
-              <Link to={"/news" + previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
+              <Link to={"/news" + previous.slug} rel="prev">
+                ← {previous.title}
               </Link>
             )}
           </li>
           <li>
             {next && (
-              <Link to={"/news" + next.fields.slug} rel="next">
-                {next.frontmatter.title} →
+              <Link to={"/news" + next.slug} rel="next">
+                {next.title} →
               </Link>
             )}
           </li>
         </ul>
-      </nav>
+      </nav> */}
     </Layout>
   );
 };
@@ -73,31 +70,21 @@ export const pageQuery = graphql`
         title
       }
     }
-    markdownRemark(id: { eq: $id }) {
+    contentfulNews(id: { eq: $id }) {
       id
-      excerpt(pruneLength: 160)
-      html
-      frontmatter {
-        title
-        date(formatString: "Do MMMM YYYY")
-        description
+      title
+      createdAt(formatString: "Do MMMM YYYY")
+      newsContent {
+        raw
       }
     }
-    previous: markdownRemark(id: { eq: $previousPostId }) {
-      fields {
-        slug
-      }
-      frontmatter {
-        title
-      }
+    previous: contentfulNews(id: { eq: $previousPostId }) {
+      slug
+      title
     }
-    next: markdownRemark(id: { eq: $nextPostId }) {
-      fields {
-        slug
-      }
-      frontmatter {
-        title
-      }
+    next: contentfulNews(id: { eq: $nextPostId }) {
+      slug
+      title
     }
   }
 `;
