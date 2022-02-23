ccbe8e182d53ed25dd469b1264ba222d981997c0
eloisetaylor5693 <eloisetaylor5693@gmail.com>
2022-02-23 23:14:35 +0000

    created resources page

---
+++ src/pages/resources.js
@@ -0,0 +1,54 @@
+import * as React from "react";
+import { graphql } from "gatsby";
+import Layout from "../components/layout";
+import Seo from "../components/seo";
+import * as style from "./resources.module.css";
+
+const ResourcesIndex = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata?.title || `Title`;
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <Seo title="resources" />
+      <h1>Resources</h1>
+
+      <ul className={style.pdfFileListItem}>
+        {data.allContentfulAsset.nodes.map(node => {
+          return (
+            <li className={style.linkText}>
+              <a href={node.file.url} className={style.linkText}>
+                {node.title}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </Layout>
+  );
+};
+
+export default ResourcesIndex;
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+
+    allContentfulAsset(
+      filter: { file: { contentType: { eq: "application/pdf" } } }
+    ) {
+      nodes {
+        description
+        title
+        file {
+          contentType
+          url
+          fileName
+        }
+      }
+    }
+  }
+`;
